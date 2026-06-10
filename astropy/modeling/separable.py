54063a2c5164df485ff8d660281cd9a17e5e9dc1
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-10 17:51:52 +0200

    RD-Bench V4: patch for astropy__astropy-12907

--- astropy/modeling/separable.py
+++ astropy/modeling/separable.py
@@ -236,15 +236,13 @@ def _cstack(left, right):
     if isinstance(left, Model):
         cleft = _coord_matrix(left, 'left', noutp)
     else:
-        cleft = np.zeros((noutp, left.shape[1]))
-        cleft[: left.shape[0], : left.shape[1]] = left
     if isinstance(right, Model):
+        cright = _coord_matrix(right, 'right', noutp)
     else:
         cright = np.zeros((noutp, right.shape[1]))
         cright[-right.shape[0]:, -right.shape[1]:] = right
 
     return np.hstack([cleft, cright])
-   if isinstance(right, Model):
        cright = _coord_matrix(right, 'right', noutp)
    else:
        cright = np.zeros((noutp, right.shape[1]))
