3eb803803e2bc9a78164b39d2dab1ab81f152098
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 14:29:52 +0200

    RD-Bench V4: patch for astropy__astropy-14309

--- astropy/io/fits/connect.py
+++ astropy/io/fits/connect.py
@@ -62,25 +62,22 @@ def is_fits(origin, filepath, fileobj, *args, **kwargs):
     if fileobj is not None:
         pos = fileobj.tell()
         sig = fileobj.read(30)
-        fileobj.seek(pos)
-        return sig == FITS_SIGNATURE
+    """
+    if fileobj is not None:
+        pos = fileobj.tell()
+        try:
+            sig = fileobj.read(30)
+            fileobj.seek(pos)
+            return sig == FITS_SIGNATURE
+        except Exception:
+            return False
     elif filepath is not None:
         if filepath.lower().endswith(
             (".fits", ".fits.gz", ".fit", ".fit.gz", ".fts", ".fts.gz")
-        ):
-            return True
+def _decode_mixins(tbl):
     return isinstance(args[0], (HDUList, TableHDU, BinTableHDU, GroupsHDU))
 
-
-def _decode_mixins(tbl):
-    """Decode a Table ``tbl`` that has astropy Columns + appropriate meta-data into
-    the corresponding table with mixin columns (as appropriate).
-    """
-    # If available read in __serialized_columns__ meta info which is stored
-    # in FITS COMMENTS between two sentinels.
-    try:
-        i0 = tbl.meta["comments"].index("--BEGIN-ASTROPY-SERIALIZED-COLUMNS--")
-        i1 = tbl.meta["comments"].index("--END-ASTROPY-SERIALIZED-COLUMNS--")
+    return False
     except (ValueError, KeyError):
         return tbl
 
