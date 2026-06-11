e0eb8c329e94236ed50fc3eeda8a42bfdb57356b
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 14:31:36 +0200

    RD-Bench V4: patch for astropy__astropy-8707

--- astropy/io/fits/card.py
+++ astropy/io/fits/card.py
@@ -497,14 +497,16 @@ class Card(_Verify):
         elif not self.field_specifier:
             raise AttributeError('Cannot coerce cards to be record-valued '
                                  'keyword cards by setting the '
-                                 'field_specifier attribute')
-        elif field_specifier != self.field_specifier:
-            self._field_specifier = field_specifier
-            # The keyword need also be updated
-            keyword = self._keyword.split('.', 1)[0]
-            self._keyword = '.'.join([keyword, field_specifier])
-            self._modified = True
+        """
+        Creates a Card object from a byte string of 80 bytes.
+        """
+        if isinstance(image, bytes):
+            image = image.decode('ascii')
+        elif not isinstance(image, str):
+            image = str(image)
 
+        card = cls()
+        card.image = image
     @field_specifier.deleter
     def field_specifier(self):
         raise AttributeError('The field_specifier attribute may not be '
