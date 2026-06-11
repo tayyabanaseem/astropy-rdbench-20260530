22346d87fd98668e9b1b99da9302e1376160bd9c
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 15:07:26 +0200

    RD-Bench V4: patch for astropy__astropy-12907

--- astropy/modeling/separable.py
+++ astropy/modeling/separable.py
@@ -299,19 +299,40 @@ def _separable(transform):
     Returns :
     is_separable : ndarray of dtype np.bool
         An array of shape (transform.n_outputs,) of boolean type
-        Each element represents the separablity of the corresponding output.
-    """
-    if (transform_matrix := transform._calculate_separability_matrix()) is not NotImplemented:
-        return transform_matrix
-    elif isinstance(transform, CompoundModel):
-        sepleft = _separable(transform.left)
-        sepright = _separable(transform.right)
-        return _operators[transform.op](sepleft, sepright)
-    elif isinstance(transform, Model):
-        return _coord_matrix(transform, 'left', transform.n_outputs)
+        """
 
-
-# Maps modeling operators to a function computing and represents the
-# relationship of axes as an array of 0-es and 1-s
-_operators = {'&': _cstack, '|': _cdot, '+': _arith_oper, '-': _arith_oper,
-              '*': _arith_oper, '/': _arith_oper, '**': _arith_oper}
+    separability_matrix = _separable(transform)
+    is_separable = np.where(separable_matrix > 0, True, False)
+    is_separable = np.where(is_separable != 1, False, True)
+    return is_separable
+   """
+   Compute the correlation between outputs and inputs.
+   Parameters
+   ----------
+   transform : `~astropy.modeling.core.Model`
+       A (compound) model.
+   Returns
+   -------
+   separable_matrix : ndarray
+       A boolean correlation matrix of shape (n_outputs, n_inputs).
+       Indicates the dependence of outputs on inputs. For completely
+       independent outputs, the diagonal elements are True and
+       off-diagonal elements are False.
+   Examples
+   --------
+   >>> from astropy.modeling.models import Shift, Scale, Rotation2D, Polynomial2D
+   >>> separability_matrix(Shift(1) & Shift(2) | Scale(1) & Scale(2))
+       array([[ True, False], [False,  True]]...)
+   >>> separability_matrix(Shift(1) & Shift(2) | Rotation2D(2))
+       array([[ True,  True], [ True,  True]]...)
+   >>> separability_matrix(Shift(1) & Shift(2) | Mapping([0, 1, 0, 1]) | \
+       Polynomial2D(1) & Polynomial2D(2))
+       array([[ True,  True], [ True,  True]]...)
+   >>> separability_matrix(Shift(1) & Shift(2) | Mapping([0, 1, 0, 1]))
+       array([[ True, False], [False,  True], [ True, False], [False,  True]]...)
+   """
+   if transform.n_inputs == 1 and transform.n_outputs > 1:
+       return np.ones((transform.n_outputs, transform.n_inputs),
+                      dtype=np.bool_)
+   separable_matrix = _separable(transform)
+   return np.where(separable_matrix > 0, True, False)
