16a2c29ecc56d16a728f7b805fe318c7364d78fc
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 14:29:37 +0200

    RD-Bench V4: patch for astropy__astropy-14182

--- astropy/io/ascii/rst.py
+++ astropy/io/ascii/rst.py
@@ -64,3 +64,12 @@ class RST(FixedWidth):
         lines = super().write(lines)
         lines = [lines[1]] + lines + [lines[1]]
         return lines
+    """
+    header_class = SimpleRSTHeader
+
+        super().__init__(delimiter_pad=None, bookend=False)
+        if header_rows is not None:
+            self.header.header_rows = header_rows
+        
+    def write(self, lines):
+        lines = super().write(lines)
