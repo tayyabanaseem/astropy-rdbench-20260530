82468b8f556ccea7632cce2e2d7acc2ccbc7857f
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 14:12:46 +0200

    RD-Bench V4: patch for astropy__astropy-13033

--- astropy/timeseries/core.py
+++ astropy/timeseries/core.py
@@ -77,16 +77,16 @@ class BaseTimeSeries(QTable):
             elif self.colnames[:len(required_columns)] != required_columns:
 
                 raise ValueError("{} object is invalid - expected '{}' "
+            if len(self.colnames) == 0:
+
+                if not self._required_columns_relax:
+                    raise ValueError("{} object is invalid - expected {} "
+                                     "as the first column{} but time series has no columns"
+                                     .format(self.__class__.__name__, required_columns, plural))
+
+            elif self.colnames[:len(required_columns)] != required_columns:
+
+                raise ValueError("{} object is invalid - required {} "
                                  "as the first column{} but found '{}'"
-                                 .format(self.__class__.__name__, required_columns[0], plural, self.colnames[0]))
-
-            if (self._required_columns_relax
-                    and self._required_columns == self.colnames[:len(self._required_columns)]):
-                self._required_columns_relax = False
-
-    @contextmanager
-    def _delay_required_column_checks(self):
-        self._required_columns_enabled = False
-        yield
-        self._required_columns_enabled = True
-        self._check_required_columns()
+                                 .format(self.__class__.__name__, required_columns, plural,
+                                         self.colnames[:len(required_columns)]))
