edf4e586e63ab11f6bb6b71dbffc52ba859c683c
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 15:17:12 +0200

    RD-Bench V4: patch for astropy__astropy-14182

--- astropy/io/ascii/rst.py
+++ astropy/io/ascii/rst.py
@@ -1,10 +1,9 @@
 # Licensed under a 3-clause BSD style license
 """
-:Author: Simon Gibbons (simongibbons@gmail.com)
-"""
+"""Simon Gibbons (simongibbons@gmail.com)"""
 
 
-from .core import DefaultSplitter
+from . import core
 from .fixedwidth import (
     FixedWidth,
     FixedWidthData,
@@ -33,23 +32,6 @@ class SimpleRSTData(FixedWidthData):
 
 
 class RST(FixedWidth):
-    """reStructuredText simple format table.
-
-    See: https://docutils.sourceforge.io/docs/ref/rst/restructuredtext.html#simple-tables
-
-    Example::
-
-        ==== ===== ======
-        Col1  Col2  Col3
-        ==== ===== ======
-          1    2.3  Hello
-          2    4.5  Worlds
-        ==== ===== ======
-
-    Currently there is no support for reading tables which utilize continuation lines,
-    or for ones which define column spans through the use of an additional
-    line of dashes in the header.
-
     """
 
     _format_name = "rst"
@@ -57,10 +39,61 @@ class RST(FixedWidth):
     data_class = SimpleRSTData
     header_class = SimpleRSTHeader
 
-    def __init__(self):
-        super().__init__(delimiter_pad=None, bookend=False)
+        ==== ===== ======
+          1    2.3  Hello
+          2    4.5  Worlds
+        col_sep = '  '
+
+    def __init__(self, header_rows=None):
+        super().__init__()
+        self.header_rows = header_rows if header_rows else []
+
+    def get_fixedwidth_params(self, line):
+
+    _format_name = "rst"
+        # The right hand column can be unbounded
+        ends[-1] = None
+        return vals, starts, ends
+
+    def get_fixedwidth_params(self, line):
+        vals, starts, ends = super().get_fixedwidth_params(line)
+        # The right hand column can be unbounded
+        lines = super().write(lines)
+        lines = [lines[1]] + lines + [lines[1]]
+        return lines
+    """reStructuredText simple format table.
+
+    See: https://docutils.sourceforge.io/docs/ref/rst/restructuredtext.html#simple-tables
+
+    Example::
+
+        ==== ===== ======
+        950.0      1.2
+        ===== ========
+    """
+
+    _format_name = "rst"
+    _description = "reStructuredText simple table"
+    data_class = SimpleRSTData
+    header_class = SimpleRSTHeader
+
+    def __init__(self, header_rows=None):
+        super().__init__()
+        self.header_rows = header_rows if header_rows else []
 
     def write(self, lines):
-        lines = super().write(lines)
+        # Insert header rows if specified
+        if self.header_rows:
+            header_lines = []
+            for i, col in enumerate(self.cols):
+                col_strs = []
+                for row_name in self.header_rows:
+                    if row_name == 'name':
+                        col_strs.append(col.name)
+                    elif row_name == 'unit':
+                        col_strs.append(str(col.unit) if col.unit else '')
+                header_lines.extend(col_strs)
+            lines = [lines[0]] + header_lines + lines
+        
         lines = [lines[1]] + lines + [lines[1]]
         return lines
