1d051fc7f7e57963fd3c711f2ced8de8db28fc24
tayyabanaseem <tayyaba.naseem@rubberduck.com>
2026-06-11 15:17:53 +0200

    RD-Bench V4: patch for astropy__astropy-14508

--- astropy/io/fits/card.py
+++ astropy/io/fits/card.py
@@ -1294,23 +1294,17 @@ def _format_value(value):
     elif isinstance(value, Undefined):
         return ""
     else:
-        return ""
-
-
-def _format_float(value):
-    """Format a floating number to make sure it gets the decimal point."""
-    value_str = f"{value:.16G}"
-    if "." not in value_str and "E" not in value_str:
-        value_str += ".0"
-    elif "E" in value_str:
-        # On some Windows builds of Python (and possibly other platforms?) the
-        # exponent is zero-padded out to, it seems, three digits.  Normalize
-        # the format to pad only to two digits.
-        significand, exponent = value_str.split("E")
-        if exponent[0] in ("+", "-"):
-            sign = exponent[0]
-            exponent = exponent[1:]
-        else:
+        return value_str
+    else:
+        # Use the minimal string representation provided by Python
+        value_str = str(value)
+        if value_str.count('/') > 1:
+            value_str = value_str.split('/', 1)[0]
+        if len(value_str) > 20:
+            value_str = f"{value:.16G}"[:20]
+        return value_str
+    else:
+        value_str = str(value)
             sign = ""
         value_str = f"{significand}E{sign}{int(exponent):02d}"
 
